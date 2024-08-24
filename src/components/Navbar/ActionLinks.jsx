2ddb5981234ae4975f8bc1bf655198b17e7949a6
votantai3070 <votantai.07092002@gmail.com>
2024-08-24 17:19:29 +0700

    FEAT: Component Header

--- src/components/Navbar/ActionLinks.jsx
+++ src/components/Navbar/ActionLinks.jsx
@@ -14,7 +14,7 @@ const ActionLinks = () => {
 						{name: 'Profile', link: '/profile'},
 						{name: 'Information', link: '/info'},
 						{name: 'My Orders', link: '/my-orders'},
-						{name: 'Change Password', link: '/change-password'},
+						{name: 'Login'},
 					],
 				},
 			],
@@ -32,14 +32,11 @@ const ActionLinks = () => {
 									<div className="py-2">
 										<div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
 									</div>
-									<div className="bg-white p-3.5 w-full">
+									<div className="bg-white p-3.5 w-64">
 										{link.sublinks?.map((mySubLink) => (
 											<div>
-												<h1 className="text-lg font-semibold">
-													{mySubLink.Head}
-												</h1>
 												{mySubLink.sublink?.map((sl) => (
-													<li className="text-sm text-gray-600 my-2.5">
+													<li className="text-base text-gray-600 my-2.5 whitespace-nowrap">
 														<Link
 															to={sl.link}
 															className="hover:text-primary font-normal normal-case"
