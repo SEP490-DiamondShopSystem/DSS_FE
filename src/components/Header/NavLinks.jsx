931bd02186ea3d28eb8f561712bf0a8e72f2167c
votantai3070 <votantai.07092002@gmail.com>
2024-10-07 19:30:07 +0700

    Fix: Header link

--- src/components/Header/NavLinks.jsx
+++ src/components/Header/NavLinks.jsx
@@ -98,30 +98,30 @@ const NavLinks = () => {
 		// },
 	];
 
-	// const handleClick = (shape, type, diamond, jewelry) => {
-	// 	if (shape) {
-	// 		localStorage.setItem('selected', shape);
-	// 		localStorage.removeItem('jewelryType');
-	// 		localStorage.setItem('diamondChoice', 'Kim Cương');
-	// 		localStorage.removeItem('jewelryChoice');
-	// 	}
-	// 	if (type) {
-	// 		localStorage.setItem('jewelryType', type);
-	// 		localStorage.removeItem('selected');
-	// 		localStorage.removeItem('diamondChoice');
-	// 		localStorage.removeItem('jewelryChoice');
-	// 	}
-	// 	if (jewelry) {
-	// 		localStorage.setItem('jewelryChoice', jewelry);
-	// 		localStorage.removeItem('diamondChoice');
-	// 	}
-	// 	if (diamond) {
-	// 		localStorage.setItem('diamondChoice', diamond);
-	// 		localStorage.removeItem('jewelryChoice');
-	// 		localStorage.removeItem('jewelryType');
-	// 		localStorage.removeItem('selected');
-	// 	}
-	// };
+	const handleClick = (shape, type, diamond, jewelry) => {
+		if (shape) {
+			localStorage.setItem('selected', shape);
+			localStorage.removeItem('jewelryType');
+			localStorage.setItem('diamondChoice', 'Kim Cương');
+			localStorage.removeItem('jewelryChoice');
+		}
+		if (type) {
+			localStorage.setItem('jewelryType', type);
+			localStorage.removeItem('selected');
+			localStorage.removeItem('diamondChoice');
+			localStorage.removeItem('jewelryChoice');
+		}
+		if (jewelry) {
+			localStorage.setItem('jewelryChoice', jewelry);
+			localStorage.removeItem('diamondChoice');
+		}
+		if (diamond) {
+			localStorage.setItem('diamondChoice', diamond);
+			localStorage.removeItem('jewelryChoice');
+			localStorage.removeItem('jewelryType');
+			localStorage.removeItem('selected');
+		}
+	};
 
 	return (
 		<>
@@ -160,48 +160,48 @@ const NavLinks = () => {
 																<a
 																	href={sl.link}
 																	className="hover:text-primary font-normal normal-case"
-																	// onClick={() => {
-																	// 	if (
-																	// 		mySubLink.Head ===
-																	// 		'Mua Kim Cương Theo Hình Dạng'
-																	// 	) {
-																	// 		handleClick(
-																	// 			sl.name,
-																	// 			null,
-																	// 			null,
-																	// 			null
-																	// 		);
-																	// 	} else if (
-																	// 		mySubLink.Head ===
-																	// 		'Thiết Kế Trang Sức Của Bạn'
-																	// 	) {
-																	// 		handleClick(
-																	// 			null,
-																	// 			sl.name,
-																	// 			null,
-																	// 			null
-																	// 		);
-																	// 	} else if (
-																	// 		sl.name ===
-																	// 		'Trang Sức Kim Cương Đính Sẵn'
-																	// 	) {
-																	// 		handleClick(
-																	// 			null,
-																	// 			null,
-																	// 			null,
-																	// 			sl.name
-																	// 		);
-																	// 	} else if (
-																	// 		sl.name === 'Kim Cương'
-																	// 	) {
-																	// 		handleClick(
-																	// 			null,
-																	// 			null,
-																	// 			sl.name,
-																	// 			null
-																	// 		);
-																	// 	}
-																	// }}
+																	onClick={() => {
+																		if (
+																			mySubLink.Head ===
+																			'Mua Kim Cương Theo Hình Dạng'
+																		) {
+																			handleClick(
+																				sl.name,
+																				null,
+																				null,
+																				null
+																			);
+																		} else if (
+																			mySubLink.Head ===
+																			'Thiết Kế Trang Sức Của Bạn'
+																		) {
+																			handleClick(
+																				null,
+																				sl.name,
+																				null,
+																				null
+																			);
+																		} else if (
+																			sl.name ===
+																			'Trang Sức Kim Cương Đính Sẵn'
+																		) {
+																			handleClick(
+																				null,
+																				null,
+																				null,
+																				sl.name
+																			);
+																		} else if (
+																			sl.name === 'Kim Cương'
+																		) {
+																			handleClick(
+																				null,
+																				null,
+																				sl.name,
+																				null
+																			);
+																		}
+																	}}
 																>
 																	{sl.name}
 																</a>
